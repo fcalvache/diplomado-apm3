12ff53f8c57a9b03e7d735af73e5c88635ff3f89
fabio calvache <fcalvache@hotmail.com>
2015-11-06 20:40:01 -0500

    bower components

---
+++ modulo_8/lab1/gulpfile.js
@@ -0,0 +1,37 @@
+var gulp = require('gulp'),
+	cssmin = require('gulp-cssmin'),
+    rename = require('gulp-rename'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify');
+    concatCss = require('gulp-concat-css');
+
+gulp.task('concat-css', function(){
+    return gulp.src(['css/styles.css'])
+        .pipe(concatCss('styles.css'))
+        .pipe(gulp.dest('min/'));
+});
+
+gulp.task('minify-css', ['concat-css'], function(){
+  return gulp.src('min/styles.css')
+    .pipe(cssmin())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('concat-files', function(){
+    gulp.src('js/*.js')
+        .pipe(concat('lab3.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('build/'))
+});
+
+gulp.task('all', ['concat-css', 'minify-css', 'concat-files'], function(){});
+
+
+var htmlmin = require('gulp-htmlmin');
+ 
+gulp.task('minifyhtml', function() {
+  return gulp.src('test.html')
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('disthtml'))
+});
